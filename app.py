62a86b00062a659ad7471b13d770306c59c43265
KeneK345 <kenzo.igarashi@aluno.ifsp.edu.br>
2025-06-24 13:59:21 -0300

    Create modelling skeleton

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from routes.usuarios import usuarios_bp
+
+app = Flask(__name__)
+app.register_blueprint(usuarios_bp)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
