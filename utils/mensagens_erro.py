006ba377f28cf2e72108c4c7b69a498ffe6a8d39
KeneK345 <kenzo.igarashi@aluno.ifsp.edu.br>
2025-06-24 16:43:29 -0300

    Implement possible user services errors into utils error messages

--- utils/mensagens_erro.py
+++ utils/mensagens_erro.py
@@ -1,1 +1,10 @@
-ERROS = {}
+ERROS = {
+    "EMAIL_DUPLICADO": {
+        "mensagem": "Email já cadastrado",
+        "status": 409
+    },
+    "USUARIO_NAO_ENCONTRADO": {
+        "mensagem": "Usuário não encontrado",
+        "status": 404
+    }
+}
